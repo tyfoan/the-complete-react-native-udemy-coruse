fd863982d4be5a5108a00f9f72793a6eedfb9f89
tyfoan <alexanderogurtsov@Alexanders-MacBook-Pro.local>
2017-11-15 00:53:00 +0200

    added layoutAnimation

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
@@ -13,9 +18,27 @@ const styles = {
 
 class ListItem extends Component {
 
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text>
+            {library.description}
+          </Text>
+        </CardSection>
+      );
+    }
+    return null;
+  }
+
   render() {
     const { titleStyle } = styles;
-    const { library: { title, id }, selectLibrary } = this.props;
+    const { library: { title, id, description }, selectLibrary, expanded } = this.props;
     return (
       <TouchableWithoutFeedback
         onPress={() => selectLibrary(id)}
@@ -26,10 +49,23 @@ class ListItem extends Component {
               { title }
             </Text>
           </CardSection>
+          {
+            expanded &&
+            <CardSection>
+              <Text style={{ flex: 1 }}>
+                {description}
+              </Text>
+            </CardSection>
+          }
         </View>
       </TouchableWithoutFeedback>
     );
   }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
